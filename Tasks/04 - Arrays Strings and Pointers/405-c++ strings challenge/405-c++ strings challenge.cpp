cf5f7effbf3492b092157621e748d5add3c9f2b6
(s) Eliraz Atia <eliraz.atia@students.plymouth.ac.uk>
2023-10-13 15:56:37 +0100

    Removed unused code from previous attempts

--- Tasks/04 - Arrays Strings and Pointers/405-c++ strings challenge/405-c++ strings challenge.cpp
+++ Tasks/04 - Arrays Strings and Pointers/405-c++ strings challenge/405-c++ strings challenge.cpp
@@ -33,63 +33,6 @@ int main()
     string matchingAttributeValue = postAttributeKeyString.substr(0, endQuotationLocation);
 
     cout << "Attribute value" << matchingAttributeValue;
-    //;char* matchSubstring = strpos(input, match);
-
-    //cout << "matching for " << match << endl;
-    //cout << "matching substring is " << matchSubstring;
 
     return 0;
-    /*
-    //Write solution here
-    bool isDetermined = false;
-    int index = 0;
-    int stringLength = size(input);
-
-    int existingAttributePointer = 0;
-    char existingAttribute[64] = "";
-    memset(existingAttribute, 0, sizeof(existingAttribute));
-
-    for (char* s = &input[0]; *s != '\0'; s++) {
-        printf("%c\n", *s);
-
-        if (s[0] == '\40') {
-            cout << "is character break" << endl;
-            cout << "checking attribute " << existingAttribute;
-            strcpy_s(existingAttribute, "");
-            existingAttributePointer = 0;
-        }
-        else {
-            cout << "is adding " << s[0];
-            existingAttribute[existingAttributePointer] = s[0];
-            existingAttributePointer += 1;
-        }
-    }
-
-    while (index < stringLength && !isDetermined) {
-        char character[1] = { input[index] };
-        index += 1;
-
-        if (strncmp(character, " ", sizeof(character))) {
-            //cout << "is character break";
-            //cout << existingAttribute << endl;
-            strcpy_s(existingAttribute, "");
-            existingAttributePointer = 0;
-        }
-        else {
-            //cout << "is adding " << existingAttribute[existingAttributePointer];
-            existingAttribute[existingAttributePointer] = character[0];
-            existingAttributePointer += 1;
-        }
-    }
-
-    // Challenge 2 - word count
-    // Type in a sentence, and count the number of time the words "the" appears 
-    getline(std::cin, input);   //Read a line (users types this in)
-    istringstream iss(input);   //Create a string "stream" (sequence of words)
-    string word;                //This will hold the next word
-    while (iss >> word) {       //Read the next word (if there is one)
-        cout << word << endl;   //Output each word in turn
-    }
-    */
 }
-
